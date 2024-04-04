33e16adee2ca4942a9ac66e88a515d21c84cf8e6
brightkams <ohakambright@gmail.com>
2024-04-04 17:11:09 +0100

    initial commit to include footer section into pages

---
+++ foodBlogApp/footer.php
@@ -0,0 +1,47 @@
+<?php
+
+echo '
+<footer class="footer-wrapper position-fixed bottom-0 w-100 border container-fluid  p-2  ">
+
+<nav class="navbar navbar-expand-lg container-fluid  justify-content-between bg-body-white ">
+    <div class="container-fluid nav-navigations row justify-content-between pe-0  ">
+
+        <div class="  d-flex col-8 align-items-center" id="navbarNav">
+            <a class="navbar-brand " href="#">
+                <p class="logo d-flex align-items-center justify-content-center m-auto p-2 text-center">Taste
+                    9ja
+                </p>
+            </a>
+            <ul class="navbar-nav ">
+                <li class="nav-item px-2 mx-2">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item px-2 mx-2 ">
+                    <a class="nav-link" href="#">Get Inspired</a>
+                </li>
+                <li class="nav-item px-2 mx-2 ">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+
+            </ul>
+        </div>
+
+
+        <div
+            class="user-access-wrapper col-2 flex-column flex-md-row container d-flex justify-content-end pe-0">
+            <button class="btn user-btn bg-dark rounded-5 m-2 p-3 text-white">
+                Sign up</button>
+            <button class="btn user-btn bg-dark rounded-5 m-2 p-3 text-white"> Login</button>
+        </div>
+    </div>
+
+
+
+</nav>
+
+</footer>
+';
+
+
+
+?>
